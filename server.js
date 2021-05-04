4af6d2f5ed86e1fb3f09a5842f3e23057390ecc0
TheKingDimvol <the_king_dimvol@mail.ru>
2021-05-05 00:32:51 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,30 +9,6 @@ app.use(express.json())
 // BodyParser для Form
 app.use(express.urlencoded({extended: false}))
 
-/*
-// Функция принимает любой запрос, печатает его метод и адрес источника
-// Если адрес источник это localhost, то разрешает ему воспользоваться API 
-// И передает запрос дальше
-app.use(function (req, res, next) {
-    if (req.headers.origin && req.headers.origin.includes('127.0.0.1')) {
-        // Разрешает запрос с данного адреса
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-
-        // Разрешает все методы для запроса
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true)
-    }
-
-    next()
-})
-*/
-
 // Neo4j-driver для обработки запросов связанный с базой данных
 app.use((req, res, next) => {
     req.neo4j = require('./middlewares/neo4j')
