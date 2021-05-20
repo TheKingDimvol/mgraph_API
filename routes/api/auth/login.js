c60f7edece2db2a4205de19c215849f291d942e9
TheKingDimvol <the_king_dimvol@mail.ru>
2021-05-20 18:12:38 +0500

    ConfirmUserInput --> userAuthValidation
    Переместил валидацию данных для Login'а

--- routes/api/auth/login.js
+++ routes/api/auth/login.js
@@ -1,11 +1,9 @@
 const { authenticate } = require('../../../controllers/Users/login');
-const { confirmUserInput } = require('../../../middlewares/confirmUserInput');
+const { confirmUserInput } = require('../../../middlewares/inputValidation/userAuthValidation');
 const router = require('express').Router();
 
 
 router.post('/', confirmUserInput, authenticate)
-
-
 
 
 module.exports = router
